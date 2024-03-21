6752c4fa0b3b17df4c693e6efbfb19308eb22ba9
유명렬 <dlek567@gmail.com>
2024-03-21 14:11:23 +0900

    fix: 주문목록 api 수정

--- backend/route/order.js
+++ backend/route/order.js
@@ -89,15 +89,14 @@ order.get("/order-list/:user_id", async (req, res) => {
 });
 
 // 주문 목록 디테일 보기
-order.get("/order-list-detail/:user_id", async (req, res) => {
-  const user_id = req.params.user_id;
+order.get("/order-list-detail/:order_id", async (req, res) => {
+  const order_id = req.params.order_id;
   try {
-    const query = `SELECT odl.order_detail_id, odl.order_id, odl.order_quentity, odl.order_progress, odl.product_id
-          FROM order_detail_list odl 
-          JOIN order_list ol ON ol.order_id = odl.order_id 
-          WHERE ol.user_id = '1'`;
-    const results = await pool.query(query, user_id);
-
+    const query = `SELECT order_detail_id, order_id, order_quentity, order_progress, product_id
+          FROM order_detail_list
+          WHERE order_id = ? `;
+    const results = await pool.query(query, [order_id]);
+    console.log(results);
     for (let i = 0; i < results[0].length; i++) {
       const product_id = results[0][i].product_id;
       const query2 = `SELECT product_name, product_price FROM product_list WHERE product_id = ?`;
--- frontend/kind-mulja/dev-dist/sw.js
+++ frontend/kind-mulja/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.he5ie8eljs"
+    "revision": "0.duinlkm76ho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
